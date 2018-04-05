266b4d232a3300dddddd0735e5707fd5fd9b08a1
mart-dominguez <mdomingu@gmail.com>
2018-04-05 08:20:39 -0300

    version inicial

---
+++ src/died/guias/guia02/app/Hola2.java
@@ -0,0 +1,25 @@
+package died.guias.guia02.app;
+
+/**
+ * 
+ * @author marti
+ * 
+ * 
+ * 
+ */
+
+public class Hola2 {
+
+	/**
+	 * 
+	 * @param args
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println("el tiempo actual en milis es " + System.currentTimeMillis());
+
+	}
+
+}
